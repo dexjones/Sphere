bbaa05af2684d6fbc459a9bd4187dd2ddc9d5359
Dexter <dexter.t.jones@outlook.com>
2024-07-28 16:03:11 -0600

    Completed Project

--- wireSphere.js
+++ wireSphere.js
@@ -13,7 +13,7 @@ var normalsArray = [];
 
 var near = -10;
 var far = 10;
-var radius = 6.0;
+var radius = 1.5;
 var theta = 0.0;
 var phi = 0.0;
 var dr = 5.0 * Math.PI / 180.0;
@@ -23,30 +23,34 @@ var right = 2.0;
 var top = 2.0;
 var bottom = -2.0;
 
-var modelViewMatrix, projectionMatrix, normalMat;
+var modelViewMatrix, projectionMatrix, normalMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc, normalMatrixLoc;
 var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
-var lightPosition = vec3(1.0, 1.0, 1.0);
-var lightAmbient = vec3(0.2, 0.2, 0.2);
-var lightDiffuse = vec3(1.0, 1.0, 1.0);
-var lightSpecular = vec3(1.0, 1.0, 1.0);
+var lightPosition = vec4(1.0, 1.0, 1.0, 1.0); // Front light
+var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+var lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+var lightSpecular = vec4(1.0, 1.0, 1.0, 1.0);
 
-var materialAmbient = vec3(1.0, 0.0, 1.0);
-var materialDiffuse = vec3(1.0, 0.8, 0.0);
-var materialSpecular = vec3(1.0, 1.0, 1.0);
-var materialShininess = 100.0;
+var materialAmbient = vec4(0.0, 1.0, 1.0, 1.0);  
+var materialDiffuse = vec4(0.0, 1.0, 1.0, 1.0);  
+var materialSpecular = vec4(1.0, 1.0, 1.0, 1.0);
+var materialShininess = 80.0;
+
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
 
 function triangle(a, b, c) {
     positionsArray.push(a);
     positionsArray.push(b);
     positionsArray.push(c);
 
-    normalsArray.push(a[0], a[1], a[2]);
-    normalsArray.push(b[0], b[1], b[2]);
-    normalsArray.push(c[0], c[1], c[2]);
+    normalsArray.push(vec4(a[0], a[1], a[2], 0.0));
+    normalsArray.push(vec4(b[0], b[1], b[2], 0.0));
+    normalsArray.push(vec4(c[0], c[1], c[2], 0.0));
 
     index += 3;
 }
@@ -87,10 +91,10 @@ window.onload = function init() {
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    var va = vec4(0.0, 0.0, -1.0, 1);
-    var vb = vec4(0.0, 0.942809, 0.333333, 1);
-    var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-    var vd = vec4(0.816497, -0.471405, 0.333333, 1);
+    var va = vec4(0.0, 0.0, -1.0, 1.0);
+    var vb = vec4(0.0, 0.942809, 0.333333, 1.0);
+    var vc = vec4(-0.816497, -0.471405, 0.333333, 1.0);
+    var vd = vec4(0.816497, -0.471405, 0.333333, 1.0);
 
     tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
 
@@ -107,27 +111,26 @@ window.onload = function init() {
     gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
 
     var normalLoc = gl.getAttribLocation(program, "aNormal");
-    gl.vertexAttribPointer(normalLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(normalLoc, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(normalLoc);
 
     modelViewMatrixLoc = gl.getUniformLocation(program, "uModelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "uProjectionMatrix");
     normalMatrixLoc = gl.getUniformLocation(program, "uNormalMatrix");
 
-    gl.uniform3fv(gl.getUniformLocation(program, "uLightPosition"), flatten(lightPosition));
-    gl.uniform3fv(gl.getUniformLocation(program, "uLightAmbient"), flatten(lightAmbient));
-    gl.uniform3fv(gl.getUniformLocation(program, "uLightDiffuse"), flatten(lightDiffuse));
-    gl.uniform3fv(gl.getUniformLocation(program, "uLightSpecular"), flatten(lightSpecular));
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightPosition"), flatten(lightPosition));
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightAmbient"), flatten(lightAmbient));
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightDiffuse"), flatten(lightDiffuse));
+    gl.uniform4fv(gl.getUniformLocation(program, "uLightSpecular"), flatten(lightSpecular));
 
-    gl.uniform3fv(gl.getUniformLocation(program, "uMaterialAmbient"), flatten(materialAmbient));
-    gl.uniform3fv(gl.getUniformLocation(program, "uMaterialDiffuse"), flatten(materialDiffuse));
-    gl.uniform3fv(gl.getUniformLocation(program, "uMaterialSpecular"), flatten(materialSpecular));
+    gl.uniform4fv(gl.getUniformLocation(program, "uMaterialAmbient"), flatten(materialAmbient));
+    gl.uniform4fv(gl.getUniformLocation(program, "uMaterialDiffuse"), flatten(materialDiffuse));
+    gl.uniform4fv(gl.getUniformLocation(program, "uMaterialSpecular"), flatten(materialSpecular));
     gl.uniform1f(gl.getUniformLocation(program, "uShininess"), materialShininess);
 
-    document.getElementById("Button0").onclick = function() { theta += dr; };
-    document.getElementById("Button1").onclick = function() { theta -= dr; };
-    document.getElementById("Button2").onclick = function() { phi += dr; };
-    document.getElementById("Button3").onclick = function() { phi -= dr; };
+    canvas.onmousedown = handleMouseDown;
+    document.onmouseup = handleMouseUp;
+    document.onmousemove = handleMouseMove;
 
     document.getElementById("Button4").onclick = function() {
         numTimesToSubdivide++;
@@ -147,6 +150,34 @@ window.onload = function init() {
     render();
 }
 
+function handleMouseDown(event) {
+    mouseDown = true;
+    lastMouseX = event.clientX;
+    lastMouseY = event.clientY;
+}
+
+function handleMouseUp(event) {
+    mouseDown = false;
+}
+
+function handleMouseMove(event) {
+    if (!mouseDown) {
+        return;
+    }
+
+    var newX = event.clientX;
+    var newY = event.clientY;
+
+    var deltaX = newX - lastMouseX;
+    var deltaY = newY - lastMouseY;
+
+    theta += deltaX * 0.01;
+    phi += deltaY * 0.01;
+
+    lastMouseX = newX;
+    lastMouseY = newY;
+}
+
 function render() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
@@ -155,11 +186,11 @@ function render() {
 
     modelViewMatrix = lookAt(eye, at, up);
     projectionMatrix = ortho(left, right, bottom, top, near, far);
-    normalMat = normalMatrix(modelViewMatrix, true);
+    normalMatrix = transpose(inverse(modelViewMatrix));
 
     gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
-    gl.uniformMatrix3fv(normalMatrixLoc, false, flatten(normalMat));
+    gl.uniformMatrix4fv(normalMatrixLoc, false, flatten(normalMatrix));
 
     for (var i = 0; i < index; i += 3)
         gl.drawArrays(gl.TRIANGLES, i, 3);
